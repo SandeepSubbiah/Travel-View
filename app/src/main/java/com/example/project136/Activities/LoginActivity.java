f21e603cfc70c9def0e09d443f6f052223805759
Sandeep Subbiah <ssandeepsubbiah@gmail.com>
2023-10-06 00:59:11 -0400

    First Commit Travel App
    First Commit Travel App

---
+++ app/src/main/java/com/example/project136/Activities/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.example.project136.Activities;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.example.project136.R;
+
+public class LoginActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
